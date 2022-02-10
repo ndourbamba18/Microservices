55431b65ea58a40cb17d1919a00500cea5769c0f
ndourbamba18 <ndourbamba18@gmail.com>
2022-02-10 20:06:12 +0100

    Eureka Registry Service

---
+++ eureka-registry-service/src/test/java/com/ndourcodeur/eurekaregistryservice/EurekaRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndourcodeur.eurekaregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- user-service/src/main/java/com/ndourcodeur/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/ndourcodeur/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.ndourcodeur.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
